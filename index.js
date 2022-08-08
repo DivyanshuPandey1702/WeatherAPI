e1b6b0c0e8b14b4fc07b4b022e7f4c55d5a42088
Divyanshu Pandey <divyanshupandey1702@gmail.com>
2022-08-08 16:16:18 +0530

    Weather Api using nodejs express

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const https = require("https");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+  console.log(__dirname);
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function (req, res) {
+  const apiKey = "54e04514ea2c94f9f2ffc59857a400a0";
+  const unit = "metric";
+  const city = req.body.cityName;
+  const url =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    city +
+    "&units=" +
+    unit +
+    "&appid=" +
+    apiKey;
+  var weatherData;
+
+  https
+    .get(url, (response) => {
+      response.on("data", function (data) {
+        weatherData = JSON.parse(data);
+        console.log(weatherData);
+        const name = weatherData.name;
+        const temp = weatherData.main.temp;
+        const img =
+          "http://openweathermap.org/img/wn/" +
+          weatherData.weather[0].icon +
+          "@2x.png";
+        res.write(
+          "<h1>The temperature in " +
+            name +
+            " is " +
+            temp +
+            " degree celsius.</h1>"
+        );
+        const description =
+          "<p>The weather is currently " +
+          weatherData.weather[0].description +
+          "</p>";
+        res.write(description);
+        res.write("<img src='" + img + "'></img>");
+        res.send();
+      });
+    })
+    .on("error", (e) => {
+      console.error(e);
+    });
+});
+
+app.listen(3000, function () {
+  console.log("Server started at port 3000.");
+});
